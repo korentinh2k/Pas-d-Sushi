e1be78465af6ad87a320ea347bd11725f25b1440
korentinh2k <45078622+korentinh2k@users.noreply.github.com>
2018-11-18 21:47:38 +0100

    Add files via upload

---
+++ pasdsushis/public/js/commande.js
@@ -0,0 +1,436 @@
+const horsdoeuvre = [
+  {
+    name: "Soupe Miso, A2",
+    id: "A1",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fmiso.png?1542560597222"
+  },
+  
+  {
+    name: "Salade de choux, A2",
+    id: "A2",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fsalade-de-choux.png?1542560597583"
+  },
+  
+  {
+    name: "Riz Nature, A3",
+    id: "A3",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Friz-nature.png?1542560596148"
+  },
+  {
+    name: "Riz Vinaigré, A4",
+    id: "A4",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Friz-vinaigre.png?1542560591129"
+  },
+  {
+    name: "Riz cantonnais, A5",
+    id: "A5",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Friz-cantonnais.png?1542560589874"
+  },
+  {
+    name: "Nouilles sautées, A6",
+    id: "A6",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fnouilles.png?1542560596218"
+  },
+  {
+    name: "Beignets de poulet, A7",
+    id: "A7",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fbeignets-poulet.png?1542560589707"
+  },
+  {
+    name: "Chirachi avocat, A8",
+    id: "A8",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fchirachi.png?1542560591866"
+  },
+  {
+    name: "Nems poulet, A9",
+    id: "A9",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fnems.png?1542560597437"
+  },
+  {
+    name: "Raviolis pékinois au poulet, A10",
+    id: "A10",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fraviolis-pekinois.jpg?1542560589359"
+  },
+  {
+    name: "Gingembre mariné, A11",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FA11.png?1542560598072"
+  }
+  ];
+
+  const yakitori = [
+  {
+    name: "Poulet, Y1",
+    id: "Y1",
+    quantity: 0,
+    description:"brochette de poulet",
+    path: "http://www.sushiq02.fr/image/cache/data/BP-640x450.png"
+    },
+    {
+    name: "Boulette de Poulet, Y2",
+    id: "Y2",
+    quantity: 0,
+    description:"brochette de Boulette de Poulet",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fyakitoriboulettes.png?1542560595920"
+    },
+    {
+    name: "Ailes de Poulet, Y3",
+    id: "Y3",
+    quantity: 0,
+    description:"brochette d'Ailes de Poulet",
+    path: "http://app.sushijardin.fr/image/cache/data/B5-640x450.png"
+    },
+    {
+    name: "Boeuf fromage, Y4",
+    id: "Y4",
+    quantity: 0,
+    description:"brochette de fromage entouré de lamelles de Boeuf",
+    path: "http://www.msushithionville.fr/image/cache/data/Yakitori6-640x450.png"
+    },
+    {
+    name: "Boeuf, Y5",
+    id: "Y5",
+    quantity: 0,
+    description:"brochette de Boeuf",
+    path: "http://app.ashinoma16.fr/image/cache/data/Y5-640x450.png"
+    },
+    {
+    name: "Saumon, Y6",
+    id: "Y6",
+    quantity: 0,
+    description:"brochette de Saumon",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fyakitorisaumon.png?1542560590098"
+    }
+    ];
+
+
+   const maki = [
+  {
+    name: "Saumon, M1",
+    id: "M1",
+    quantity: 0,
+    description:"maki au saumon",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fmakisaumon.png?1542560589385"
+  },
+  {
+    name: "Maki concombre, M2",
+    id: "M2",
+    quantity: 0,
+    description:"Maki au concombre",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fmakiconcombre.jpg?1542560596671"
+  },
+  {
+    name: "Saumon Avocat, M3",
+    id: "M3",
+    quantity: 0,
+    description:"Le meilleur des makis, juste avec du cheese",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FSR1.jpg?1542560591599"
+  },
+  {
+    name: "Saumon cheese crousitllant, M4",
+    id: "M4",
+    quantity: 0,
+    description:"cheese, salmon, crousti",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fmakisaumoncheesecrousti.jpg?1542560597132"
+  },
+  {
+    name: "Surimi avocat, M5",
+    id: "M5",
+    quantity: 0,
+    description:"Sur Rémi, l'avocat !",
+    path: "http://www.justice.gouv.fr/art_pix/X-201708281533576111.jpg"
+  },
+  {
+    name: "Cheese, M6",
+    id: "M6",
+    quantity: 0,
+    description:"Le meilleur des makis, juste avec du cheese",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fmakicheese.png?1542560594317"
+  },
+  {
+    name: "Avocat, M7",
+    id: "M7",
+    quantity: 0,
+    description:"Le meilleur des makis, juste avec du cheese",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fmakiavocat.png?1542560593988"
+  },
+  {
+    name: "Crevettes, M8",
+    id: "M8",
+    quantity: 0,
+    description:"Le meilleur des makis, juste avec du cheese",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fmakicrevette.png?1542560594477"
+  },
+  {
+    name: "Las Vegas, M9",
+    id: "M9",
+    quantity: 0,
+    description:"Le meilleur des makis, juste avec du cheese",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Flasvegas.jpg?1542560590598"
+  },
+  {
+    name: "Thon cuit avocat, M10",
+    id: "M10",
+    quantity: 0,
+    description:"maki au saumon",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fmakithoncuit.png?1542560594956"
+  }
+  ];
+
+
+ const sushi = [
+  {
+    name: "Saumon, S1",
+    id: "S1",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FS1.png?1542560591391"
+  },
+ 
+  {
+    name: "Daurade, S2",
+    id: "S2",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FS2.png?1542560589459"
+  },
+  {
+    name: "Crevette, S3",
+    id: "S3",
+    quantity: 0,
+    description:"",
+    path: "https://i0.wp.com/sushigm.com/wp-content/uploads/2017/06/20161209024506.jpg?fit=640%2C410"
+  },
+  {
+    name: "Omelette, S4",
+    id: "S4",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fomelette.png?1542560590836"
+  },
+  {
+    name: "Avocat, S5",
+    id: "S5",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2Fsushiavocat.png?1542560597820"
+  }
+  ];
+
+   const sashimi = [
+   {
+    name: "Saumon, SA1",
+    id: "SA1",
+    quantity: 0,
+    description:"simples tranches de Saumon frais",
+    path: "https://png.pngtree.com/element_origin_min_pic/16/10/14/235800fd6d96521.jpg"
+    },
+    {
+    name: "Thon, SA2",
+    id: "SA2",
+    quantity: 0,
+    description:"simples tranches de Thon frais",
+    path: "http://www.sushidream.fr/produit/1176_154.png"
+    },
+    {
+    name: "Daurade, SA3",
+    id: "SA3",
+    quantity: 0,
+    description:"simples tranches de Daurade fraiche",
+    path: "http://www.sushidor-dijon.com/147-thickbox_default/sa4-sashimi-daurade.jpg"
+    },
+    {
+    name: "Assortiment, SA4",
+    id: "SA4",
+    quantity: 0,
+    description:"simples tranche de Saumon, Daurade et Thon frais",
+    path: "http://www.sushivillage15.fr/image/cache/data/7-640x450.png"
+    }
+    ];
+
+ const neigerolls = [
+    {
+    name: "Thon, M11",
+    id: "M11",
+    quantity: 0,
+    description:"Neige roll au Thon, un morceau de thon entouré par une couche de riz rond cuit.(sans algue)",
+    path: "http://app.newhanjo.fr/image/cache/data/420-640x450.png"
+    },
+    {
+    name: "Saumon, M12",
+    id: "M12",
+    quantity: 0,
+    description:"Neige roll au Saumon, un morceau de Saumon entouré par une couche de riz rond cuit.(sans algue)",
+    path: "http://app.newhanjo.fr/image/cache/data/421-640x450.png"
+    },
+    {
+    name: "Cheese, M13",
+    id: "M13",
+    quantity: 0,
+    description:"Neige roll au Fromage, un morceau de fromage entouré par une couche de riz rond cuit.(sans algue)",
+    path: "http://www.awabi14.fr/image/cache/data/F24-600x600.png"
+    },
+    {
+    name: "Avocat, M14",
+    id: "M14",
+    quantity: 0,
+    description:"Neige roll à l'Avocat, un morceau d'Avocat entouré par une couche de riz rond cuit.(sans algue)",
+    path: "http://hokisushi78.fr/images/carte/makin3.png"
+    }
+   ];
+
+  const springrolls = [
+  {
+    name: "Avocat thon cuit, M15",
+    id: "M15",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FM15.png?1542560592556"
+  },
+ 
+  {
+    name: "Saumon avocat, M16",
+    id: "M16",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FM16.png?1542560596481"
+  },
+  {
+    name: "Crevette avocat, M17",
+    id: "M17",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FM17.png?1542560592799"
+  },
+  {
+    name: "Saumon cheese, M18",
+    id: "M18",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FM18.png?1542560593005"
+  },
+  {
+    name: "Avocat cheese, M19",
+    id: "M19",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FM19.png?1542560593236"
+  },
+  {
+    name: "Concombre cheese, M20",
+    id: "M20",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FM20.png?1542560593758"
+  }
+  ];
+
+  const salmonrolls = [
+  {
+    name: "Saumon avocat, SR1",
+    id: "SR1",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FSR1.jpg?1542560591599"
+  },
+  {
+    name: "Concombre, SR2",
+    id: "SR2",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FSR2.jpg?1542560589572"
+  },
+  {
+    name: "Saumon cheese, SR3",
+    id: "SR3",
+    quantity: 0,
+    description:"",
+    path: "https://cdn.glitch.com/be1cc56d-122b-42a1-970c-93f761441af3%2FSR3.png?1542560595497"
+  }
+  ];
+
+  const specialrolls = [
+  {
+    name: "Soupe Miso",
+    id: "S",
+    quantity: 0,
+    description:"",
+    path: "../assets/compote.png"
+  },
+  {
+    name: "Nems",
+    quantity: 0,
+    description:"",
+    path: "../assets/sand.png"
+  }
+  ];
+
+const vm = new Vue({
+  el: "#page",
+  data: {
+    tab:0,
+    items: [],
+    sushi: sushi,
+    yakitori: yakitori,
+    salmonrolls: salmonrolls,
+    springrolls: springrolls,
+    neigerolls: neigerolls,
+    specialrolls: specialrolls,
+    horsdoeuvre: horsdoeuvre,
+    maki: maki,
+    sashimi: sashimi,
+    showCart: false,
+    verified: false,
+    quantity: 1
+  },
+  computed: {
+  
+  },
+  methods: {
+    addToCart(item) {
+      item.quantity += 1;
+      if (!this.items.includes(item))
+      {
+        this.items.push(item);
+      }
+    },
+    EnvoiCommande()
+    {
+      for(var j = 0; j<this.items.length; j++) 
+        { 
+                console.log('ID : ' + this.items[j].id + ', Value : ' + this.items[j].quantity);
+                  //JsonObject.jsonkey += this.items[j].quantity;
+        }
+        
+    },
+    removeFromCart(item) {
+      item.quantity = 0;
+      this.items.splice(this.items.indexOf(item), 1);
+    }
+  }
+});
+
+
+      
+       
